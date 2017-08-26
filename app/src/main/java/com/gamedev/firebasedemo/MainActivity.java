ae403da92d22763bb0f5de175628bf35b4c0258d
6ameDev <sumit.das@go-jek.com>
2017-08-26 15:44:19 +0700

    Cleaned up MainActivity

--- app/src/main/java/com/gamedev/firebasedemo/MainActivity.java
+++ app/src/main/java/com/gamedev/firebasedemo/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,18 +17,20 @@ import javax.inject.Inject;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "FCMessaging";
+    public static final String TOPIC = "test_topic";
+
     @Inject
     RemoteConfig remoteConfig;
-    private TextView usernameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ((FirebaseDemoApp) getApplication()).deps().inject(this);
+        subscribeToFcmTopic();
         remoteConfig.init();
 
-        usernameText = (TextView) findViewById(R.id.text_username);
+        final TextView usernameText = (TextView) findViewById(R.id.text_username);
 
         Button refreshButton = (Button) findViewById(R.id.btn_refresh);
         refreshButton.setOnClickListener(new OnClickListener() {
@@ -44,36 +45,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button forceRefreshButton = (Button) findViewById(R.id.btn_force_refresh);
-        forceRefreshButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                remoteConfig.fetch(new RemoteConfig.Callback() {
-                    @Override
-                    public void onFinished() {
-                        usernameText.setText(remoteConfig.username());
-                    }
-                });
-            }
-        });
-
-        final EditText topicInput = (EditText) findViewById(R.id.input_topic);
-        Button subscribeButton = (Button) findViewById(R.id.btn_subscribe);
-        subscribeButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String topic = topicInput.getText().toString();
-                FirebaseMessaging.getInstance().subscribeToTopic(topic);
-
-                String msg = String.format("[Subscribed] [Topic: %s]", topic);
-                Log.d(TAG, msg);
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-            }
-        });
-
         // Handle possible data accompanying notification message.
         Bundle bundle = getIntent().getExtras();
         Executor executor = RemoteMessageParser.parse(bundle);
         executor.execute(remoteConfig);
+    }
+
+    private void subscribeToFcmTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC);
+
+        String msg = String.format("[Subscribed] [Topic: %s]", TOPIC);
+        Log.d(TAG, msg);
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 }
